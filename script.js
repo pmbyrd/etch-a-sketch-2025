79273dc5d8130330a63d08b819687eb9474051bd
pmbyrd <pmbyrd@gmail.com>
2025-06-13 16:22:43 -0700

    feat: function to create board reworked function to create squares dynamically reworked

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ let num = 16;
 
 function createBoard(num) {
   //NOTE I am creating a square so x and y can be the same value, having them be different added complication
-  const board = document.querySelector(".board")
+  const board = document.querySelector(".board");
   for (let i = 0; i < num; i++) {
-    let row = document.createElement("div")
-    board.appendChild(row)
     for (let j = 0; j < num; j++) {
-      let column = document.createElement("div") 
-      row.appendChild(column)
+      const sq = createSquare(num);
+      board.appendChild(sq);
     }
   }
-  console.log("hello")
+  console.log("hello");
 }
 
-createBoard(num)
+//NOTE the squares need to be created via javascript and styling added here since size needs to be dynamic hard coding it in is a bit beyond my abilities at the moment
+function createSquare(num) {
+  const sq = document.createElement("div");
+  const percent = 100 / num;
+  sq.style.border = "1px solid black";
+  sq.style.backgroundColor = "teal";
+  sq.style.width = `${percent}%`;
+  sq.style.height = `${percent}%`;
+  sq.style.boxSizing = "border-box";
+  return sq;
+}
+
+createBoard(num);
 function displayBoard(num) {
   //NOTE this function will be added to the button to implement the board on the page
 }
